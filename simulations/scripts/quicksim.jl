1f5962b4fb1282bf447afe52f7fd15d620b225fa
Salvador Balkus <salbalkus@gmail.com>
2024-07-08 20:25:20 -0400

    revert change breaking oracledensity

--- simulations/scripts/quicksim.jl
+++ simulations/scripts/quicksim.jl
@@ -46,7 +46,7 @@ plotparams = Dict("mtpname" => config["mtpname"], "nreps" => config["nreps"], "n
 Plots.savefig(plotsdir(savename(plotparams, allowedtypes = (Real, String, Symbol, Vector))) * ".png")
 
 ### Non-IID ###
-name = "quicksimnet.jl"
+name = "synthetic-linear-er.jl"
 
 # Generate ground truth
 include(scriptsdir("dgp", "$(name)")) # load `dgp` and `intervention`
@@ -62,7 +62,7 @@ config["nreps"] = 500
 config["mtp"] = mtp
 config["bootstrap"] = BasicSampler()
 config["bootstrap_samples"] = 0
-config["mtpname"] = "comparison3"
+config["mtpname"] = "comparison4"
 
 # Run the simulation
 @time result = networkMTPsim.simulate(config; print_every = config["nreps"] ÷ 10)
@@ -73,3 +73,31 @@ makeplots(result, config; ci = [true, false, false], methodnames = ["tmle", "plu
 plotparams = Dict("mtpname" => config["mtpname"], "nreps" => config["nreps"], "name" => name, "samples" => config["samples"])
 Plots.savefig(plotsdir(savename(plotparams, allowedtypes = (Real, String, Symbol, Vector))) * ".png")
 
+config = dict_list(config)[1]
+config["data"] = simulate_data(config)["data"]
+config["i"] = 1
+using Random
+Random.seed!(config["seed"])
+foo = simulate_mtp_fit(config, true)
+Random.seed!(config["seed"])
+foo2 = simulate_mtp_fit(config, false)
+
+config["data"]
+
+iid = true
+data = iid ? CausalTables.replace(config["data"]; summaries = (;)) : config["data"]
+mtpmach = machine(config["mtp"], data, config["intervention"]) |> fit!
+fitted_params(nuisance_machines(mtpmach).machine_mean)
+report(mtpmach).LAs
+
+r2 = report(mtpmach)
+scatter(r.Qδn, r2.Qδn)
+scatter(r.Hn, r2.Hn)
+
+mtpmach = machine(config["mtp"], data, config["intervention"]) |> fit!
+
+
+# Compute causal estimates
+result = ModifiedTreatment.estimate(mtpmach, config["intervention"])
+ModifiedTreatment.bootstrap!(config["bootstrap"], result, config["bootstrap_samples"])
+est = getestimate(result)
