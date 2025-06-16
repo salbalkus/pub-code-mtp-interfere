1dc9c893390f3ff9d1d89bc96afe892f938552f2
Salvador Balkus <salbalkus@gmail.com>
2025-06-16 17:38:27 -0400

    remove files

--- simulations/scripts/dgp/semisynthetic-trunc.jl
+++ simulations/scripts/dgp/semisynthetic-trunc.jl
@@ -27,34 +27,37 @@ L = Tables.matrix(df)[:, 1:16]  # Select the first 16 columns as confounders for
 #reg2 = neighbors * L * β   # Adjust reg2 to account for neighbors
 reg = vec(sum(L, dims=2))  # Use the sum of confounders as the regression term
 reg2 = vec(sum(neighbors * L, dims=2))
-μ = 0
 
-many_distributions = DataGeneratingProcess(
-    [Symbol("L", i) for i in 1:size(L, 2)],
-    [O -> Dirac.(L[:,i]) for i in 1:size(L, 2)]
-)
+function semisynthetic_scm(α, σ)
 
-many_summaries = DataGeneratingProcess(
-    vcat([:G], [Symbol("L", i, "s") for i in 1:size(L, 2)]),
-    vcat([O -> neighbors], [O -> Sum(Symbol("L", i), :G) for i in 1:size(L, 2)])
-)
+    many_distributions = DataGeneratingProcess(
+        [Symbol("L", i) for i in 1:size(L, 2)],
+        [O -> Dirac.(L[:,i]) for i in 1:size(L, 2)]
+    )
 
-many_variables = merge(many_distributions, many_summaries)
+    many_summaries = DataGeneratingProcess(
+        vcat([:G], [Symbol("L", i, "s") for i in 1:size(L, 2)]),
+        vcat([O -> neighbors], [O -> Sum(Symbol("L", i), :G) for i in 1:size(L, 2)])
+    )
 
-σ = 1.0
-final_output = @dgp(
-    F $ Friends(:G),
-    A ~ (@. Normal(reg .+ μ, (reg + 1))),
-    As $ Sum(:A, :G),
-    Y ~ (@. truncated(Normal(A + As + reg + reg2 - 150, σ), A + As + reg + reg2 - 150 - (6*σ), A + As + reg + reg2 - 150 + (6*σ)))
-)
+    many_variables = merge(many_distributions, many_summaries)
 
-confoundersymbs = vcat([Symbol("L$(i)s") for i in 1:16], [Symbol("L$(i)") for i in 1:16], [:F])
-scm = StructuralCausalModel(
-    CausalTables.merge(many_variables, final_output),
-    treatment=[:A, :As],
-    response= :Y,
-    causes = (A = confoundersymbs, As = confoundersymbs, Y = vcat([:A, :As], confoundersymbs)),
-)
+    final_output = @dgp(
+        F $ Friends(:G),
+        A ~ (@. Normal(reg .+ α, (reg + 1))),
+        As $ Sum(:A, :G),
+        Y ~ (@. truncated(Normal(A + As + reg + reg2 - 150, σ), A + As + reg + reg2 - 150 - (6*σ), A + As + reg + reg2 - 150 + (6*σ)))
+    )
+
+    confoundersymbs = vcat([Symbol("L$(i)s") for i in 1:16], [Symbol("L$(i)") for i in 1:16], [:F])
+    scm = StructuralCausalModel(
+        CausalTables.merge(many_variables, final_output),
+        treatment=[:A, :As],
+        response= :Y,
+        causes = (A = confoundersymbs, As = confoundersymbs, Y = vcat([:A, :As], confoundersymbs)),
+    )
+
+    return scm
+end
 
 intervention = AdditiveShift(1.0)
--- simulations/scripts/semisynthetic.jl
+++ simulations/scripts/semisynthetic.jl
@@ -9,28 +9,24 @@ name = "semisynthetic-trunc.jl"
 include(scriptsdir("dgp", "$(name)")) # load `dgp` and `intervention`
 ntruth = n
 samples = [n]
-config = @strdict name ntruth scm intervention
-
-# Compute the truth assuming an additive intervention
 add_shift = intervention.δb(nothing)
-ψ = mean(((reg .+ μ) .+ add_shift) .* (1.0 .+ vec(sum(neighbors, dims=1))) .+ reg .+ reg2 .- 200)
-#reps = 10000
-#σ2 = mean([var(conmean(scm, rand(scm, 1), :Y)) for i in 1:reps]) / n
 
+### Νο change in mean of treatment ###
+α = 0.0
+netname = "α=$(α)"
+scm = semisynthetic_scm(α, 1.0)
+config = @strdict name ntruth scm intervention
+config["ψ"] = mean(((reg .+ α) .+ add_shift) .* (1.0 .+ vec(sum(neighbors, dims=1))) .+ reg .+ reg2 .- 150)
+config["eff_bound"] = NaN
 
-config["ψ"] = ψ
-#config["ψ_dif"] = mean(ψdifs)
-config["eff_bound"] = NaN#σ2
-netname = "finite_Amean=$(μ)"#"reps=$(reps)"
 
 # Correctly-specified linear model
 include(scriptsdir("auxiliary", "2-run-simulation-linear.jl"))
-
 tbl = opchars(result, config; varsymb = :σ2net, methodnames = ["tmle", "tmle_iid", "ols"])
 tbl2 = DataFrames.select(tbl, [:method, :bias, :pct_bias, :variance, :coverage, :ci_width])
 
 # Super Learning
-netname = "approx=$(k);super"
+netname = "α=$(α);super"
 include(scriptsdir("auxiliary", "2-run-simulation.jl"))
 
 tbl_super = opchars(result, config; varsymb = :σ2net, methodnames = ["tmle", "tmle_iid", "ols"])
@@ -38,9 +34,60 @@ tbl2_super = DataFrames.select(tbl_super, [:method, :bias, :pct_bias, :variance,
 
 tbl2_super[!, "method"] = tbl2_super[!, "method"] .* "_super"
 tbl_final = vcat(tbl2, tbl2_super[1:2, :])
-
 output_path = joinpath(projectdir(), "..", "data", "semisynthetic-summary.csv")
 CSV.write(output_path, tbl_final)
+
+### Decrease mean of treatment ###
+α = -5.0
+netname = "α=$(α)"
+scm = semisynthetic_scm(α, 1.0)
+config = @strdict name ntruth scm intervention
+config["ψ"] = mean(((reg .+ α) .+ add_shift) .* (1.0 .+ vec(sum(neighbors, dims=1))) .+ reg .+ reg2 .- 150)
+config["eff_bound"] = NaN
+
+
+# Correctly-specified linear model
+include(scriptsdir("auxiliary", "2-run-simulation-linear.jl"))
+tbl = opchars(result, config; varsymb = :σ2net, methodnames = ["tmle", "tmle_iid", "ols"])
+tbl2 = DataFrames.select(tbl, [:method, :bias, :pct_bias, :variance, :coverage, :ci_width])
+
+# Super Learning
+netname = "α=$(α);super"
+include(scriptsdir("auxiliary", "2-run-simulation.jl"))
+
+tbl_super = opchars(result, config; varsymb = :σ2net, methodnames = ["tmle", "tmle_iid", "ols"])
+tbl2_super = DataFrames.select(tbl_super, [:method, :bias, :pct_bias, :variance, :coverage])
+
+tbl2_super[!, "method"] = tbl2_super[!, "method"] .* "_super"
+tbl_final = vcat(tbl2, tbl2_super[1:2, :])
+output_path = joinpath(projectdir(), "..", "data", "semisynthetic-summary.csv")
+CSV.write(output_path, tbl_final)
+
+### Increase mean of treatment ###
+α = 5.0
+netname = "α=$(α)"
+scm = semisynthetic_scm(α, 1.0)
+config = @strdict name ntruth scm intervention
+config["ψ"] = mean(((reg .+ α) .+ add_shift) .* (1.0 .+ vec(sum(neighbors, dims=1))) .+ reg .+ reg2 .- 150)
+config["eff_bound"] = NaN
+
+# Correctly-specified linear model
+include(scriptsdir("auxiliary", "2-run-simulation-linear.jl"))
+tbl = opchars(result, config; varsymb = :σ2net, methodnames = ["tmle", "tmle_iid", "ols"])
+tbl2 = DataFrames.select(tbl, [:method, :bias, :pct_bias, :variance, :coverage, :ci_width])
+
+# Super Learning
+netname = "α=$(α);super"
+include(scriptsdir("auxiliary", "2-run-simulation.jl"))
+
+tbl_super = opchars(result, config; varsymb = :σ2net, methodnames = ["tmle", "tmle_iid", "ols"])
+tbl2_super = DataFrames.select(tbl_super, [:method, :bias, :pct_bias, :variance, :coverage])
+
+tbl2_super[!, "method"] = tbl2_super[!, "method"] .* "_super"
+tbl_final = vcat(tbl2, tbl2_super[1:2, :])
+output_path = joinpath(projectdir(), "..", "data", "semisynthetic-summary.csv")
+CSV.write(output_path, tbl_final)
+
 
 
 
